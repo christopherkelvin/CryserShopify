0c865c122e4ecef2ab7f58e7825409d03ca87278
Christopher Kelvin <christopherkelvin100@gmail.com>
2024-11-22 20:55:59 +0300

    Added the data file

---
+++ app/utils/product.js
@@ -0,0 +1,82 @@
+import ProdImage from "../assets/images/VegLogo.png";
+import ProdImage2 from "../assets/images/Fresh-Apples.webp";
+import ProdImage3 from "../assets/images/Rice.jpeg";
+import ProdImage4 from "../assets/images/Eggs.jpeg";
+import ProdImage5 from "../assets/images/Bread.jpeg";
+import ProdImage6 from "../assets/images/Milk.jpeg";
+import ProdImage7 from "../assets/images/0il.jpeg";
+import ProdImage8 from "../assets/images/Banana.jpeg";
+import ProdImage9 from "../assets/images/Wing.jpeg";
+import ProdImage10 from "../assets/images/Cake.jpeg";
+export const Product = [
+  {
+    id: 1,
+    name: "Green Vegetables",
+    price: 5000,
+    description: "Contains 1 cabbage and 3 carrots",
+    image: ProdImage,
+  },
+  {
+    id: 2,
+    name: "Fresh Apples",
+    price: 7000,
+    description: "Pack of 6 fresh and juicy apples",
+    image: ProdImage2,
+  },
+  {
+    id: 3,
+    name: "Premium Rice",
+    price: 25000,
+    description: "5kg bag of high-quality basmati rice",
+    image: ProdImage3,
+  },
+  {
+    id: 4,
+    name: "Organic Eggs",
+    price: 12000,
+    description: "Dozen of fresh, organic eggs",
+    image: ProdImage4,
+  },
+  {
+    id: 5,
+    name: "Whole Wheat Bread",
+    price: 4500,
+    description: "Freshly baked whole wheat bread loaf",
+    image: ProdImage5,
+  },
+  {
+    id: 6,
+    name: "Milk Pack",
+    price: 9000,
+    description: "2 liters of fresh, full-cream milk",
+    image: ProdImage6,
+  },
+  {
+    id: 7,
+    name: "Cooking Oil",
+    price: 18000,
+    description: "1 liter of premium vegetable oil",
+    image: ProdImage7,
+  },
+  {
+    id: 8,
+    name: "Bananas",
+    price: 4000,
+    description: "Bunch of 8 ripe bananas",
+    image: ProdImage8,
+  },
+  {
+    id: 9,
+    name: "Chicken Wings",
+    price: 15000,
+    description: "1kg of fresh chicken wings",
+    image: ProdImage9,
+  },
+  {
+    id: 10,
+    name: "Chocolate Cake",
+    price: 25000,
+    description: "1kg of delicious chocolate cake",
+    image: ProdImage10,
+  },
+];
