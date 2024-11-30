23401b7317effffc5f2611073411ce1d83814de1
Christopher Kelvin <christopherkelvin100@gmail.com>
2024-11-30 14:18:26 +0300

    made the product page

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,4 +1,11 @@
+import { json, useLoaderData } from "@remix-run/react"
+import { ItemsMiddle } from "~/components/itemsmiddle";
 
+import {Product} from "../utils/product"
+export const loader = async () => {
+    return json(Product)
+}
 export default function Products() {
-    return <>Hello</>
+    const data = useLoaderData();
+    return <ItemsMiddle data={ data} />;
 }
